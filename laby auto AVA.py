ea8276112a98aa0985bb2539d741809de5558d11
ava-solal <130062672+ava-solal@users.noreply.github.com>
2023-05-08 17:47:43 +0200

    Add files via upload

--- laby auto AVA.py
+++ laby auto AVA.py
@@ -1,66 +1,36 @@
 import random
 
-# longueur
-ROWS = 10
-# largeur
-COLS = 10
+# Dimensions du labyrinthe
+largeur_labyrinthe = 20
+hauteur_labyrinthe = 20
 
-lab = []
+# Caractères pour représenter les murs, les chemins et les points
+mur = '1'
+chemin = '0'
 
-for i in range(ROWS*2+1):
-    lab.append([])
-    for j in range(COLS*2+1):
-        if i == 0 or j == 0 or i == ROWS*2 or j == COLS*2:
-            #  bord du labyrinthe
-            lab[i].append("1")
-        elif i % 2 == 0 and j % 2 == 0:
-            # intersection
-            lab[i].append("0")
-        elif i % 2 == 0:
-            # ligne horizontale
-            if random.random() < 0.5:
-                lab[i].append("1")
-            else:
-                lab[i].append("0")
-        elif j % 2 == 0:
-            # ligne vertical
-            if random.random() < 0.5:
-                lab[i].append("1")
-            else:
-                lab[i].append("0")
-        else:
-            # open space
-            lab[i].append("0")
-#direction possible
-voisin = {"N": (-1, 0), "S": (1, 0), "E": (0, 1), "O": (0, -1)}
+# Fonction pour générer un labyrinthe aléatoire
+def generer_labyrinthe():
+    labyrinthe = [[mur] * (2 * largeur_labyrinthe + 1) for _ in range(2 * hauteur_labyrinthe + 1)]
 
-def avoir_voisin(row, col):
-    voisin = []
-    for direction, (drow, dcol) in voisin.items():
-        if 0 <= row + drow < ROWS*2+1 and 0 <= col + dcol < COLS*2+1:
-            voisin.append((row + drow, col + dcol, direction))
-    return voisin
+    def creuser(x, y):
+        labyrinthe[y][x] = chemin
 
+        directions = [(0, -2), (0, 2), (-2, 0), (2, 0)]
+        random.shuffle(directions)
 
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
 
-def generer_lab():
-    row, col = random.randint(0, ROWS*2-1), random.randint(0, COLS*2-1)
-    stack = [(row, col)]
+            if 0 <= nx < 2 * largeur_labyrinthe + 1 and 0 <= ny < 2 * hauteur_labyrinthe + 1 and labyrinthe[ny][nx] == mur:
+                labyrinthe[ny - dy // 2][nx - dx // 2] = chemin
+                creuser(nx, ny)
 
-    while stack:
-        row, col = stack.pop()
-        lab[row][col] = "1"
-        voisin = avoir_voisin(row, col)
-        random.shuffle(voisin)
-        for nrow, ncol, direction in voisin:
-            if lab[nrow][ncol] == "1":
-                continue
-            enlever_mur(row, col, nrow, ncol)
-            stack.append((nrow, ncol))
+    creuser(0, 0)
+    return labyrinthe
 
-generer_lab()
-
-for row in lab:
-    print("".join(row))
+# Afficher le labyrinthe généré
+labyrinthe = generer_labyrinthe()
+for ligne in labyrinthe:
+    print(' '.join(ligne))
 
 
